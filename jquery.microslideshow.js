4aefdfc48bb3abe66af98193b956d91fa9934866
Pierre Aboucaya <p@asper.fr>
2017-05-31 12:22:12 +0200

    Indentation

--- jquery.microslideshow.js
+++ jquery.microslideshow.js
@@ -1,93 +1,93 @@
 (function($){
   $.fn.microslideshow = function(action, options) {
-  	
-  	if($.type(action) == 'object'){
-  	  options = action;
-  	  action = 'init';
-  	}
-  	
-  	if(!action){
-  		action = 'init';
-  	}
-	        
-  	return this.each(function(i, el){
-  		
-  		var $el = $(el);
-  		var data = $el.data();
-  		
-  		if(action == 'init'){
-  			
-  			if(!options){
-  				options = {};
-  			}
+    
+    if($.type(action) == 'object'){
+      options = action;
+      action = 'init';
+    }
+    
+    if(!action){
+      action = 'init';
+    }
+          
+    return this.each(function(i, el){
+      
+      var $el = $(el);
+      var data = $el.data();
+      
+      if(action == 'init'){
+        
+        if(!options){
+          options = {};
+        }
         data.microslideshow = {
-        	current: 0,
-        	timer: null,
-        	slides: $el.children(),
-        	options : $.extend({
-	            autoplay: true,
-	            duration: 1500,
-	            delay: 5000,
-	            transition: 'fade'
-	        }, options)
+          current: 0,
+          timer: null,
+          slides: $el.children(),
+            options : $.extend({
+            autoplay: true,
+            duration: 1500,
+            delay: 5000,
+            transition: 'fade'
+          }, options)
         };
         data.microslideshow.slides.each(function(slideIndex, slide){
-              if(slideIndex){
-                  $(slide).detach();
-              }
+          if(slideIndex){
+            $( slide).detach();
+          }
         });
         if(data.microslideshow.options.autoplay){
-            $el.trigger('microslideshow.beforeInitialPlay');
-            $el.microslideshow('play');
-            $el.trigger('microslideshow.afterInitialPlay');
+          $el.trigger('microslideshow.beforeInitialPlay');
+          $el.microslideshow('play');
+          $el.trigger('microslideshow.afterInitialPlay');
         }
         $el.trigger('microslideshow.afterInitialize');
-	        
-  		} else if(action == 'play'){
-  			
+          
+      } else if(action == 'play'){
+        
         $el.microslideshow('pause');
         $el.trigger('microslideshow.beforePlay');
         var nextFunction = function(){
-        	$el.microslideshow('next');
-        	data.microslideshow.timer = setTimeout(nextFunction, data.microslideshow.options.delay);
+          $el.microslideshow('next');
+          data.microslideshow.timer = setTimeout(nextFunction, data.microslideshow.options.delay);
         };
         data.microslideshow.timer = setTimeout(nextFunction, data.microslideshow.options.delay);
         $el.trigger('microslideshow.afterPlay');
             
-  		} else if(action == 'pause'){
-  			
+      } else if(action == 'pause'){
+        
         $el.trigger('microslideshow.beforePause');
         clearInterval(data.microslideshow.timer);  
         $el.trigger('microslideshow.afterPause');
-  			
-  		} else if(action == 'next'){
-  			
-  			var next = data.microslideshow.current + 1;
-  			if(next == data.microslideshow.slides.length){
-  				next = 0;
-  			}
+        
+      } else if(action == 'next'){
+        
+        var next = data.microslideshow.current + 1;
+        if(next == data.microslideshow.slides.length){
+          next = 0;
+        }
         $el.trigger('microslideshow.beforeNext');
         $el.microslideshow('show', next); 
         $el.trigger('microslideshow.afterNext');
-  			
-  		} else if(action == 'prev') {
-  			
-  			var prev = data.microslideshow.current - 1;
-  			if(prev < 0){
-  				prev = data.microslideshow.slides.length - 1;
-  			}
+        
+      } else if(action == 'prev') {
+        
+        var prev = data.microslideshow.current - 1;
+        if(prev < 0){
+          prev = data.microslideshow.slides.length - 1;
+        }
         $el.trigger('microslideshow.beforePrev');
         $el.microslideshow('show', prev); 
         $el.trigger('microslideshow.afterPrev');
             
-  		} else if(action == 'show') {
-  			
-  			var slideNumber = options;
-  			if(data.microslideshow.current == slideNumber || !data.microslideshow.slides.eq(slideNumber)){
-  				return;
-  			}
-  			var $current = data.microslideshow.slides.eq(data.microslideshow.current);
-  			$current.stop();
+      } else if(action == 'show') {
+        
+        var slideNumber = options;
+        if(data.microslideshow.current == slideNumber || !data.microslideshow.slides.eq(slideNumber)){
+          return;
+        }
+        var $current = data.microslideshow.slides.eq(data.microslideshow.current);
+        $current.stop();
         $el.trigger('microslideshow.beforeShow', [slideNumber]);
         var $slide = data.microslideshow.slides.eq(slideNumber);
         if('fade' == data.microslideshow.options.transition){
@@ -112,12 +112,11 @@
             $el.trigger('microslideshow.afterShow');
           }});
         }
-  			
-  		} else if(action == 'slide') {
-  			
-  			var slideNumber = options;
-  			var current = data.microslideshow.current;
-  			var length = data.microslideshow.slides.length;
+      } else if(action == 'slide') {
+        
+        var slideNumber = options;
+        var current = data.microslideshow.current;
+        var length = data.microslideshow.slides.length;
         if(!slideNumber || slideNumber == 'current'){
           slideNumber = current;
         }
@@ -144,8 +143,8 @@
           return false;
         }
         return $slide;
-  			
-  		}
+        
+      }
     });
   };
 }(jQuery));
